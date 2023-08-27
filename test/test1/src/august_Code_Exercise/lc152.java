f13e00b6660837dfe94f7a9946384ae2e479b382
du-yang-1024 <1825391430@qq.com>
2023-08-27 20:58:56 +0800

    lc 8.27

---
+++ test/test1/src/august_Code_Exercise/lc152.java
@@ -0,0 +1,20 @@
+package august_Code_Exercise;
+
+
+
+public class lc152 {
+    public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int[] f=new int[n+1];
+        int[] g=new int[n+1];
+        g[0]=f[0]=1;
+        int ret=Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int x=nums[i-1],y=f[i-1] * nums[i-1],z=g[i-1] * nums[i-1];
+            f[i]=Math.max(x,Math.max(y,z));
+            g[i]=Math.min(x,Math.min(y,z));
+            ret = Math.max(ret,f[i]);
+        }
+        return ret;
+    }
+}
---
+++ test/test1/src/august_Code_Exercise/lc918.java
@@ -0,0 +1,19 @@
+package august_Code_Exercise;
+
+public class lc918 {
+    public int maxSubarraySumCircular(int[] nums) {
+        int n=nums.length;
+        int[] f=new int[n+1];
+        int[] g=new int[n+1];
+        int sum=0,fmax=Integer.MIN_VALUE,gmin=Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int x=nums[i-1];
+            f[i]=Math.max(x,x+f[i-1]);
+            fmax=Math.max(fmax,f[i]);
+            g[i]=Math.min(x,x+g[i-1]);
+            gmin=Math.min(gmin,g[i]);
+            sum+=x;
+        }
+        return sum==gmin?fmax:Math.max(fmax,sum-gmin);
+    }
+}
